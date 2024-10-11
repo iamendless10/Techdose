b96de055af83a09897cc3879ae32c4346d03c8e5
iamendless10 <kayalennian10@gmail.com>
2024-10-11 17:57:59 +0530

    Data Stream Problems added

---
+++ Array/Search_in_roated_sortedarray.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0, r = nums.size() - 1;
+
+        while (l <= r) {
+            int mid = l + (r - l) / 2; 
+
+            if (nums[mid] == target) {
+                return mid; 
+            }
+
+            
+            if (nums[l] <= nums[mid]) {
+                if (nums[l] <= target && target < nums[mid]) {
+                    r = mid - 1; //in the left 
+                } else {
+                    l = mid + 1; // in the right 
+                }
+            }
+            
+
+            else {
+                if (nums[mid] < target && target <= nums[r]) {
+                    l = mid + 1; 
+                } else {
+                    r = mid - 1; 
+                }
+            }
+        }
+
+        return -1; 
+    }
+};
