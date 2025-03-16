85ff6a24e167ac49942902cca14a5f1c8b2abda6
iamendless10 <kayalennian10@gmail.com>
2025-03-16 19:13:52 +0530

    Updated

---
+++ DP/delete-operation-for-two-strings.cpp
@@ -0,0 +1,21 @@
+class Solution {
+    private:
+        int cc(int i, int j, string &s, string &t, vector<vector<int>> &dp){
+            if(i < 0 || j < 0)  return 0;
+    
+            if(dp[i][j] != -1) return dp[i][j];
+    
+            if(s[i]==t[j]) return dp[i][j] = 1 + cc(i - 1, j - 1, s, t, dp);
+    
+            return dp[i][j] = max(cc(i - 1, j, s, t, dp),cc(i, j - 1, s, t, dp));
+    
+        }
+    public:
+        int minDistance(string word1, string word2) {
+            int n = word1.size();
+            int m = word2.size();
+            vector<vector<int>> dp(n, vector<int> (m, -1));
+            cc(n - 1, m - 1, word1, word2, dp);
+            return ((n - dp[n - 1][m - 1]) + (m - dp[n - 1][m - 1]));
+        }
+    };
---
+++ DP/unique-paths.cpp
@@ -0,0 +1,16 @@
+class Solution {
+    private:
+        int cc(int i, int j, int m, int n, vector<vector<int>> &dp){
+            if(i == m - 1 && j == n - 1) return 1;
+            if(i > m || j > n) return 0;
+    
+            if(dp[i][j] != -1) return dp[i][j];
+    
+            return dp[i][j] = cc(i + 1, j, m, n, dp) + cc(i, j + 1, m, n, dp);
+        }
+    public:
+        int uniquePaths(int m, int n) {
+            vector<vector<int>> dp(m + 1, vector<int> (n + 1 , -1));
+            return cc(0,0,m,n,dp);
+        }
+    };
